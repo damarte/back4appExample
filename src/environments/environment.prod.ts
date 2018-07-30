4944f846dd0099747b254e9e1d41f8b4c92b6dca
David Martínez <damarte86@gmail.com>
2018-07-30 14:22:56 +0200

    Removed files

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  PARSE_APP_ID: 'oAgRe951IVqHg8ZWTt3bhET61GVFEhzKhlpUxGn8',
-  PARSE_JS_KEY: '0JajRkGxBZmQGk5yVST2dDW293MRhJLcWyTR8PXw',
-  PARSE_MASTER_KEY: 'X1Go8sJVAHB855XYWDCSWCuwlKy6yQJCD4yqpEKi',
-  serverURL: 'https://parseapi.back4app.com',
-  liveQueriesServerURL: 'wss://dmartineztest.back4app.io'
-};
